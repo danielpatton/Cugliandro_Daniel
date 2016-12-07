1aa6571f28e55d3a29ec1d6fa56704a05f0b8acd
dcugliandro <daniel.cugliandro@gmail.com>
2016-12-07 09:15:49 -0800

    Added Lesson_09

--- AP_Lesson_08/8.0/Ex_02.java
+++ AP_Lesson_08/8.0/Ex_02.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 public class Ex_02
 {
+	static String word, word1, word2, word3;
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter your first word: ");
-		String word1 = kb.next();
+		System.out.println("Please enter your word: ");
+		word1 = kb.next();
 		System.out.println("Please enter your second word: ");
-		String word2 = kb.next();
+		word2 = kb.next();
 		System.out.println("Please enter your final word: ");
-		String word3 = kb.next();
-		System.out.print(makeCenter(word1, word2, word3));
-		
+		word3 = kb.next();
+		System.out.println(makeCenter(word1));
+		System.out.println(makeCenter(word2));
+		System.out.println(makeCenter(word3));
 	}
 	
-	public static String makeCenter(String word1, String word2, String word3)
+	public static String makeCenter(String word)
 	{
-		
-		if(word.length() > 20) 
+		if(word.length() >= 20) 
 			return word;
 		else 
 		{
-			return 0 + makeCenter(" " + word + " ");
+			return makeCenter(" " + word + " ");
 			
 		}
-	}//not finished
+	}
+	
 }
--- AP_Lesson_08/8.0/Ex_04.java
+++ AP_Lesson_08/8.0/Ex_04.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 public class Ex_04
 {
+	static int start;
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter your word: ");
 		String word = kb.next();
 		int stop = word.length();
-		int start = 0;
-		System.out.print(tree(word, start, stop));
+		
+		System.out.println(tree(word, start, stop));	
 	}
 	
 	public static String tree(String word, int start, int stop)
 	{
 		if(start<=stop)
 		{
-			System.out.print(word.substring(0, start));
+			System.out.printf("\n%15s", word.substring(0, start));
 			start+=1;
-			tree(word, start, stop);
-		}	//not finished
-	}
+			return tree(word, start, stop);
+		}	
+		else
+			return " ";
+	} 
 }
