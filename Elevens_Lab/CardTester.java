46f269552a34a8d692a3608aeaa77ed863b572b4
dcugliandro <daniel.cugliandro@gmail.com>
2017-05-10 09:13:24 -0700

    Added Elevens Lab

---
+++ Elevens_Lab/CardTester.java
@@ -0,0 +1,17 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card uno = new Card("#", Hearts, "#")
+		Card due= new Card("#", Hearts, "#");
+		Card tre = new Card("#", Hearts, "#");
+		
+	}
+}
